ddd104ed801aa01c6ba5a7782d3dfa0a0ec697fb
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-02-06 22:45:24 -0300

    listagem e criação de um novo jogo concluído

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,5 +1,15 @@
-
+const games = []
 
 export const gamesController = {
+    createGame(req, res){
+        const { name, image, stockTotal, pricePerDay } = req.body
 
+        const game = {}
+        Object.assign(game, {name, image, stockTotal, pricePerDay})
+
+        return res.status(201).send()
+    },
+    getGames(req, res){
+        return res.send(games)
+    }
 }
